5841c979d26c6a3d5bcbe5f59f10420a0f1ab9b2
abraao <abraaocpm@hotmail.com>
2021-04-26 18:18:46 -0300

    ndas

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// const url = 'https://api-projeto-node.herokuapp.com/';
+const url = 'http://localhost:3001';
+
 export default axios.create({
-  baseURL: 'https://api-projeto-node.herokuapp.com/',
+  baseURL: url,
 });

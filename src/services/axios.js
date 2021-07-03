85fc0a9f0d890ac2656f161c8c986214fd73725f
abraao <abraaocpm@hotmail.com>
2021-07-03 17:41:32 -0300

    ndada

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://31.220.52.85:3001';
+const url = 'https://31.220.52.85:3001';
 // const url = 'http://localhost:3001';
 
 export default axios.create({

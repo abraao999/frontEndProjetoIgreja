2c7ee4ff30355d4eac36073835f7a0479666cdda
abraao <abraaocpm@hotmail.com>
2021-07-04 09:15:15 -0300

    nada

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'https://api-projeto-node.herokuapp.com/';
 // const url = 'https://31.220.52.85:3001';
+const url = 'https://api-projeto-node.herokuapp.com/';
 // const url = 'http://localhost:3001';
 
 export default axios.create({

5e3098eabb43129c9ce016870956899a32db3bbf
abraao999 <45611524+abraao999@users.noreply.github.com>
2021-07-17 18:59:56 -0300

    alhdasgdakhdakdhjak

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const url = 'https://31.220.52.85:3001';
 const url = 'https:///api-projeto-node.herokuapp.com/';
 // const url = 'http://localhost:3001';
-
+const ld=0
 export default axios.create({
   baseURL: url,
 });

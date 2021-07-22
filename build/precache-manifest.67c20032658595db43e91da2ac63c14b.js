c6b64a72b62296aeae1bd5b8ba2f6f68a636b646
abraao <abraaocpm@hotmail.com>
2021-07-22 19:16:50 -0300

    build

---
+++ build/precache-manifest.67c20032658595db43e91da2ac63c14b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df30bc42d9ea9c4466a41f106c0efc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a2ac58224d395fbe5db",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6a2ac58224d395fbe5db",
+    "url": "/static/js/2.8239edd8.chunk.js"
+  },
+  {
+    "revision": "830d2a445d92e56eab9be350b2702304",
+    "url": "/static/js/2.8239edd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a62a1e176249f98025",
+    "url": "/static/js/main.c5205af5.chunk.js"
+  },
+  {
+    "revision": "d8e76a3a2789f714a248",
+    "url": "/static/js/runtime-main.3f12ba25.js"
+  },
+  {
+    "revision": "9065a408f8322b7e0d54132b63363d16",
+    "url": "/static/media/logo.9065a408.png"
+  }
+]);

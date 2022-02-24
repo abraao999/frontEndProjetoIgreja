514f653b6ef68af633292485852db10d710923cc
abraao <abraaocpm@hotmail.com>
2022-02-24 15:59:24 -0300

    install webpack

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path'); // CommonJS
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+          },
+        },
+      },
+      { test: /\.txt$/, use: 'raw-loader' },
+
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ],
+  },
+  devtool: 'source-map',
+};

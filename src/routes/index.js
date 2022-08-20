330acc0967499faa1f3f6e17e4947bfd4158c83d
abraao <abraaocpm@hotmail.com>
2022-08-20 14:37:28 -0300

    correcao rotas livraria

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,71 +1,71 @@
-import React from 'react';
-import { Switch } from 'react-router-dom';
+import React from "react";
+import { Switch } from "react-router-dom";
 
-import { useSelector } from 'react-redux';
-import MyRoute from './MyRoute';
-import Aluno from '../pages/Aluno';
-import Fotos from '../pages/Fotos';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Page404 from '../pages/Page404';
-import Painel from '../pages/Painel';
-import Funcao from '../pages/AbaConfiguracoes/Funcao';
-import Cargo from '../pages/AbaConfiguracoes/Cargo';
-import Congregacao from '../pages/AbaConfiguracoes/Congregacao';
-import Classe from '../pages/AbaConfiguracoes/Classe';
-import Departamento from '../pages/AbaConfiguracoes/Departamento';
-import CadMembro from '../pages/AbaSecretaria/CadMembro';
-import ListMembros from '../pages/AbaSecretaria/ListMembros';
-import DetailMembro from '../pages/AbaSecretaria/DetailMembro';
-import Caixa from '../pages/AbaCaixa/Caixa';
-import CaixaEbd from '../pages/AbaEdb/CaixaEbd';
-import RelatorioCaixa from '../pages/AbaCaixa/RelatorioCaixa';
-import Abatimento from '../pages/AbaCaixa/Abatimento';
-import RelatorioAbatimento from '../pages/AbaCaixa/RelatorioAbatimento';
-import Dizimo from '../pages/AbaCaixa/Dizimo';
-import RelatorioDizimo from '../pages/AbaCaixa/RelatorioDizimo';
-import RelatorioDizimoGeral from '../pages/AbaCaixa/RelatorioDizimoGeral';
-import CadAluno from '../pages/AbaEdb/CadAluno';
-import ListAluno from '../pages/AbaEdb/ListAluno';
-import DetailAluno from '../pages/AbaEdb/DetailAluno';
-import Chamada from '../pages/AbaEdb/Chamada';
-import RelatorioPresencaDiaria from '../pages/AbaEdb/RelatorioPresencaDiaria';
-import RelatorioPresencaGeral from '../pages/AbaEdb/RelatorioPresencaGeral';
-import PresencaDetalhada from '../pages/AbaEdb/PresencaDetalhada';
-import ListaAniversario from '../pages/AbaSecretaria/ListaAniversario';
-import EditPass from '../pages/AbaSecretaria/EditPass';
-import RelatorioDiario from '../pages/AbaCaixa/RelatorioDiario';
-import RelatorioCaixaEbd from '../pages/AbaEdb/RelatorioCaixaEbd';
-import Configuracoes from '../pages/AbaConfiguracoes/Configuracoes';
-import Ebd from '../pages/AbaEdb/Ebd';
-import Secretaria from '../pages/AbaSecretaria/Secretaria';
-import Tesoraria from '../pages/AbaCaixa/Tesoraria';
-import Home from '../pages/Home';
-import NovoVisitante from '../pages/AbaCulto/NovoVisitante';
-import ListaVisitantes from '../pages/AbaCulto/ListaVisitantes';
-import MediaCaixa from '../pages/AbaCaixa/MediaCaixa';
-import Visitante from '../pages/AbaCulto/VIsitante';
-import PedidoOracao from '../pages/AbaCulto/PedidoOracao';
-import NovoPedido from '../pages/AbaCulto/NovoPedido';
-import ListaPedido from '../pages/AbaCulto/ListaPedido';
-import RelatorioDizimoDiario from '../pages/AbaCaixa/RelatorioDizimoDiario';
-import ControleAcesso from '../pages/AbaSecretaria/ControleAcesso';
-import ControleCarterinha from '../pages/AbaSecretaria/ControleCarterinha';
-import CadAlunoTeologia from '../pages/AbaEscolaTeologica/CadAlunoTeologia';
-import ListAlunosTeologia from '../pages/AbaEscolaTeologica/ListAlunosTeologia';
-import EscolaTeologica from '../pages/AbaEscolaTeologica/EscolaTeologica';
-import CadClasseTeologia from '../pages/AbaEscolaTeologica/CadClasseTeologia';
-import DetailAlunoTeologia from '../pages/AbaEscolaTeologica/DetailAluno';
-import TeologiaChamada from '../pages/AbaEscolaTeologica/TeologiaChamada';
-import TeologiaLivro from '../pages/AbaEscolaTeologica/TeologiaLivro';
-import MeuCadastro from '../pages/AbaPerfil/MeuCadastro';
-import Perfil from '../pages/AbaPerfil/Perfil';
-import CadLivro from '../pages/AbaLivraria/CadLivro';
-import Venda from '../pages/AbaLivraria/Venda';
-import Livraria from '../pages/AbaLivraria/Livraria';
-import PedidoLivro from '../pages/AbaLivraria/PedidoLivro';
-import ListaPedidoLivraria from '../pages/AbaLivraria/ListaPedidoLivraria';
-import Estoque from '../pages/AbaLivraria/Estoque';
+import { useSelector } from "react-redux";
+import MyRoute from "./MyRoute";
+import Aluno from "../pages/Aluno";
+import Fotos from "../pages/Fotos";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Page404 from "../pages/Page404";
+import Painel from "../pages/Painel";
+import Funcao from "../pages/AbaConfiguracoes/Funcao";
+import Cargo from "../pages/AbaConfiguracoes/Cargo";
+import Congregacao from "../pages/AbaConfiguracoes/Congregacao";
+import Classe from "../pages/AbaConfiguracoes/Classe";
+import Departamento from "../pages/AbaConfiguracoes/Departamento";
+import CadMembro from "../pages/AbaSecretaria/CadMembro";
+import ListMembros from "../pages/AbaSecretaria/ListMembros";
+import DetailMembro from "../pages/AbaSecretaria/DetailMembro";
+import Caixa from "../pages/AbaCaixa/Caixa";
+import CaixaEbd from "../pages/AbaEdb/CaixaEbd";
+import RelatorioCaixa from "../pages/AbaCaixa/RelatorioCaixa";
+import Abatimento from "../pages/AbaCaixa/Abatimento";
+import RelatorioAbatimento from "../pages/AbaCaixa/RelatorioAbatimento";
+import Dizimo from "../pages/AbaCaixa/Dizimo";
+import RelatorioDizimo from "../pages/AbaCaixa/RelatorioDizimo";
+import RelatorioDizimoGeral from "../pages/AbaCaixa/RelatorioDizimoGeral";
+import CadAluno from "../pages/AbaEdb/CadAluno";
+import ListAluno from "../pages/AbaEdb/ListAluno";
+import DetailAluno from "../pages/AbaEdb/DetailAluno";
+import Chamada from "../pages/AbaEdb/Chamada";
+import RelatorioPresencaDiaria from "../pages/AbaEdb/RelatorioPresencaDiaria";
+import RelatorioPresencaGeral from "../pages/AbaEdb/RelatorioPresencaGeral";
+import PresencaDetalhada from "../pages/AbaEdb/PresencaDetalhada";
+import ListaAniversario from "../pages/AbaSecretaria/ListaAniversario";
+import EditPass from "../pages/AbaSecretaria/EditPass";
+import RelatorioDiario from "../pages/AbaCaixa/RelatorioDiario";
+import RelatorioCaixaEbd from "../pages/AbaEdb/RelatorioCaixaEbd";
+import Configuracoes from "../pages/AbaConfiguracoes/Configuracoes";
+import Ebd from "../pages/AbaEdb/Ebd";
+import Secretaria from "../pages/AbaSecretaria/Secretaria";
+import Tesoraria from "../pages/AbaCaixa/Tesoraria";
+import Home from "../pages/Home";
+import NovoVisitante from "../pages/AbaCulto/NovoVisitante";
+import ListaVisitantes from "../pages/AbaCulto/ListaVisitantes";
+import MediaCaixa from "../pages/AbaCaixa/MediaCaixa";
+import Visitante from "../pages/AbaCulto/VIsitante";
+import PedidoOracao from "../pages/AbaCulto/PedidoOracao";
+import NovoPedido from "../pages/AbaCulto/NovoPedido";
+import ListaPedido from "../pages/AbaCulto/ListaPedido";
+import RelatorioDizimoDiario from "../pages/AbaCaixa/RelatorioDizimoDiario";
+import ControleAcesso from "../pages/AbaSecretaria/ControleAcesso";
+import ControleCarterinha from "../pages/AbaSecretaria/ControleCarterinha";
+import CadAlunoTeologia from "../pages/AbaEscolaTeologica/CadAlunoTeologia";
+import ListAlunosTeologia from "../pages/AbaEscolaTeologica/ListAlunosTeologia";
+import EscolaTeologica from "../pages/AbaEscolaTeologica/EscolaTeologica";
+import CadClasseTeologia from "../pages/AbaEscolaTeologica/CadClasseTeologia";
+import DetailAlunoTeologia from "../pages/AbaEscolaTeologica/DetailAluno";
+import TeologiaChamada from "../pages/AbaEscolaTeologica/TeologiaChamada";
+import TeologiaLivro from "../pages/AbaEscolaTeologica/TeologiaLivro";
+import MeuCadastro from "../pages/AbaPerfil/MeuCadastro";
+import Perfil from "../pages/AbaPerfil/Perfil";
+import CadLivro from "../pages/AbaLivraria/CadLivro";
+import Venda from "../pages/AbaLivraria/Venda";
+import Livraria from "../pages/AbaLivraria/Livraria";
+import PedidoLivro from "../pages/AbaLivraria/PedidoLivro";
+import ListaPedidoLivraria from "../pages/AbaLivraria/ListaPedidoLivraria";
+import Estoque from "../pages/AbaLivraria/Estoque";
 
 export default function Routes() {
   const idFuncao = useSelector((state) => state.auth.function_id);
@@ -561,56 +561,56 @@ export default function Routes() {
         component={Livraria}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute
         path="/cadLivro/:id/edit"
         component={CadLivro}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute
         path="/cadLivro/"
         component={CadLivro}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute
         path="/venda/"
         component={Venda}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute
         path="/pedidoLivro/:id/edit"
         component={PedidoLivro}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute
         path="/pedidoLivro/"
         component={PedidoLivro}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute
         path="/ListaPedidoLivraria/"
         component={ListaPedidoLivraria}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute
         path="/estoque/"
         component={Estoque}
         idFuncao={idFuncao}
         isClosed
-        usuarioPermitido={[{ id: 1, id: 13, id: 14 }]}
+        usuarioPermitido={[{ id: 1 }, { id: 13 }, { id: 14 }]}
       />
       <MyRoute path="*" component={Page404} />
     </Switch>
